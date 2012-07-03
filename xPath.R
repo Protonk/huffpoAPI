ad534ea57a62647a14dfb8decdd6d8e78e13f8a1
Adam Hyland <protonk@gmail.com>
2012-07-03 13:06:51 -0500

    sQuote is a bastard.
    allow for leading/trailing slashes in children/continuation (vs. as char vector)

--- xPath.R
+++ xPath.R
@@ -4,24 +4,24 @@
 # Children and continuation are optional
 # continuation e.g. //root[test>10]/one/two/three
 # can be added w/ a variable like continuation = c("one", "two", "three")
+# can also be specified like continuation = "one/two/three" 
 # same w/ children
 # arbitrarily many children and continuation variables can be added
 
 xPathGen <- function(root, children = NULL, comp.arg = NULL, ...) {  
-  # The paste(c("1", "2"), collapse = "/") trick is to get around
+  # The paste(c("arg1", "arg2"), collapse = "/") trick is to get around
   # unwanted behavior when what would be the 2nd arg is missing
   root <- paste0("//", root)
   children <- unlist(Filter(length, list(children)))
-  root <- paste(c(root, children), collapse = "/")
+  root <- paste(c(root, gsub("^/|/$", "", children)), collapse = "/")
   
   # Single quotes and brackets complicate this a bit
-  # make a list of the sides and operator
+  # Numbers can be single quoted too and work fine
   if (length(comp.arg) == 3) {
-    comp.arg[3] <- paste0("'", comp.arg[3], "'")
+    comp.arg[3] <- paste0("'",comp.arg[3], "'")
     comp.flat <- paste(comp.arg, collapse = "")
     root <- paste0(root, "[", comp.flat, "]")
   }
   remainder <- unlist(Filter(length, list(...)))
-  paste(c(root, remainder), collapse = "/")
+  paste(c(root, gsub("^/|/$", "", remainder)), collapse = "/")
 }
-
