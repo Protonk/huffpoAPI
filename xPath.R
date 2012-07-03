3124380f16c54ec2e8b167e960dd00a46a5e34e7
Adam Hyland <protonk@gmail.com>
2012-07-03 16:20:58 -0500

    overhaul and generalize. Can now handle any number of nodes and comparisons in an arbitrary order.

--- xPath.R
+++ xPath.R
@@ -1,27 +1,44 @@
 ### xPath Helper functions
 
-# Generate simple xPath arguments
-# Children and continuation are optional
-# continuation e.g. //root[test>10]/one/two/three
-# can be added w/ a variable like continuation = c("one", "two", "three")
-# can also be specified like continuation = "one/two/three" 
-# same w/ children
-# arbitrarily many children and continuation variables can be added
 
-xPathGen <- function(root, children = NULL, comp.arg = NULL, ...) {  
+###### xPathGen()
+### Generate simple xPath arguments
+# All child elements are optional
+# nodes must be added in order but can be added as:
+## el1 = "bob", el2 = "alice", el3 = "eve"
+## el123 = "bob/alice/eve"
+## elvector = c("bob", "alice", "eve")
+# comparisons can be added (again, in order)
+## elcomp = "c("bob", ">", "eve")
+## elcomp = "bob>20"
+# I assume the single quoted string is on the right if you
+# offfer 3 elements.
+# none of the arguments need be named. You can just pass
+# a list if you want
+
+xPathGen <- function(root, ...) {
+  genComp <- function(x) {
+    x <- unlist(x)
+    if (length(x) == 3) {
+      x[3] <- paste0("'",x[3], "'")
+      x.flat <- paste(x, collapse = "")
+    } else {
+      x.flat <- x
+    }
+    paste0("[", x.flat, "]")
+  }
+  if (!is.list(...)) {list(...)}
+  
+  # plucks out comparisons and applies genComp (formatting them properly)
+  # preserves order
+  children <- Filter(length, ...)
+  names(children) <- 1:length(children)
+  children.comp <- lapply(Filter(function(x) grep("[<>=]", x), children), genComp)
+  children[names(children.comp)] <- children.comp
+  
   # The paste(c("arg1", "arg2"), collapse = "/") trick is to get around
   # unwanted behavior when what would be the 2nd arg is missing
   root <- paste0("//", root)
-  children <- unlist(Filter(length, list(children)))
-  root <- paste(c(root, gsub("^/|/$", "", children)), collapse = "/")
-  
-  # Single quotes and brackets complicate this a bit
-  # Numbers can be single quoted too and work fine
-  if (length(comp.arg) == 3) {
-    comp.arg[3] <- paste0("'",comp.arg[3], "'")
-    comp.flat <- paste(comp.arg, collapse = "")
-    root <- paste0(root, "[", comp.flat, "]")
-  }
-  remainder <- unlist(Filter(length, list(...)))
-  paste(c(root, gsub("^/|/$", "", remainder)), collapse = "/")
+  root <- paste(c(root, gsub("^/|/$", "", unlist(children))), collapse = "/")
+  gsub("/(?=\\[)", "", root, perl = TRUE)
 }
