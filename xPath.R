5fa575b25e75cb05c36b013a269183c1f3b501a7
Adam Hyland <protonk@gmail.com>
2012-07-03 03:27:34 -0500

    start to helper functions for xPath
    Adds xPath conditional generator

---
+++ xPath.R
@@ -0,0 +1,29 @@
+### xPath Helper functions
+
+# Generate simple xPath arguments
+# Children and continuation are optional
+# continuation e.g. //root [test > 10]/one/two/three
+# can be added w/ a variable like continuation = c("one", "two", "three")
+# same w/ children
+# arbitrarily many children and continuation variables can be added
+
+
+xPathGen <- function(root, children = NULL, comp.arg = NULL,
+                     operator = NULL, value.arg = NULL, ...) {
+  # The paste(c("1", "2"), collapse = "/") trick is to get around
+  # unwanted behavior when what would be the 2nd arg is missing
+  root <- paste0("//", root)
+  children <- unlist(Filter(length, list(children)))
+  root <- paste(c(root, children), collapse = "/")
+  
+  # Single quotes and brackets complicate this a bit
+  # make a list of the sides and operator
+  if (length(comp.arg) == 3) {
+    comp.arg[3] <- paste0("'", comp.arg[3], "'")
+    comp.flat <- paste(comp.arg, collapse = " ")
+    comp.flat <- paste0(" ", "[", comp.flat, "]")
+    root <- paste0(root, comp.flat)
+  }
+  remainder <- unlist(Filter(length, list(...)))
+  paste(c(root, remainder), collapse = "/")
+}
