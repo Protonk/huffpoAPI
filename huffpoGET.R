57148af89b0e994c0623e910b62fb577d75c3aa9
Adam Hyland <protonk@gmail.com>
2012-07-02 19:52:19 -0500

    add comments
    remove library(XML) as we don't need it
    
    simplify the "US"/state issue

--- huffpoGET.R
+++ huffpoGET.R
@@ -1,11 +1,17 @@
+## function to send GET requests for charts and polls (not singular chart)
+
+# httr is not on CRAN. In order to install it you need the development tools for R
+# and package "devtools" (which is on CRAN, thankfully!)
+
+# then you can get httr w/ install_github("httr")
 library(httr)
-library(XML)
+
 
 huffpoGETmultiple <- function(path, format = "xml", state = NULL, topic = NULL, page = NULL) {
 
+# builds the path (minus queries) so we don't have to worry about it later
 huffpo.base <- "http://elections.huffingtonpost.com"
 path <- match.arg(tolower(path), c("charts", "polls"))
-
 url.premod <- switch(path,
                      charts = "pollster/api/charts",
                      polls =  "pollster/api/polls")
@@ -13,20 +19,33 @@ url.premod <- paste(url.premod, switch(format,
                                          json = ".json", 
                                          xml = ".xml"), 
                                          sep = "")
+
+# Not all arguments are required (non are, actually)
 arg.names <- character(0)
 
-if (!is.null(state) & !grepl("us", state, ignore.case = TRUE)) {
-  state <- gsub("^\\s+|\\s+$", "", state)
-  fullstate.parm <- sapply(state.name, function(y) { agrep(y, state, ignore.case = TRUE)})
-  fullstate.parm <- Filter(length, fullstate.parm)
-  if (length(fullstate.parm) > 0) {
-    state <- state.abb[state.name %in% names(fullstate.parm)]
-  } else {
-    state <- match.arg(toupper(state), state.abb)
+# State can be "US", in which case we won't find it among the state abbreviations
+# rather than add another if statement, we just add it in.
+if (!is.null(state)) {
+  # Fuzzy search for full state names
+  stateconvert <- function(state) {
+    state.abb.plus <- c(state.abb, "US")
+    state.name.plus <- c(state.name, "United States")
+    fullstate.parm <- sapply(state.name.plus, function(y) { agrep(y, state, ignore.case = TRUE)})
+    # returns only the state we asked for
+    fullstate.parm <- Filter(length, fullstate.parm)
+    # if we got a match, convert the name to the abbreviation
+    if (length(fullstate.parm) > 0) {
+      state <- state.abb.plus[state.name.plus %in% names(fullstate.parm)]
+    } else {
+      state <- match.arg(toupper(state), state.abb.plus)
+    }
   }
+  state <- gsub("^\\s+|\\s+$", "", state)
+  state <- stateconvert(state)
   arg.names <- append(arg.names, "state")
 }
 
+# Topic can be colloquial or exact.
 if (!is.null(topic)) {
   topic.args <- c("house", "senate", "governor", 
                   "president","job approval", 
@@ -37,6 +56,7 @@ if (!is.null(topic)) {
 
   topic <- gsub("^\\s+|\\s+$", "", topic)
   if (grepl("^2012|^obama", topic, ignore.case = TRUE)) {
+    # hyphens as it will eventually go into a URL
     topic <- gsub("\\s+", "-", topic)
     topic <- match.arg(tolower(topic), topic.API.value)
   } else {
@@ -46,6 +66,8 @@ if (!is.null(topic)) {
   arg.names <- append(arg.names, "topic")
 }
 
+# ignores page names otherwise takes only the first one
+# this will eventually become a proper match statement
 if (path != "polls") {
   page <- NULL
 } else {
@@ -53,9 +75,16 @@ if (path != "polls") {
   arg.names <- append(arg.names, "page")
 }
 
+# we've populated the names already. Any NULL object will not show up in
+# arg.values. If they're all NULL then just get the base query
+# we've checked twice by now but that's not really slow
 arg.values <- c(state, topic, page)
-final.query <- paste(arg.names, arg.values, sep = "=", collapse = "&")
+if (!is.null(arg.values)) {
+	final.query <- paste(arg.names, arg.values, sep = "=", collapse = "&")
+	GET(url = huffpo.base, path = url.premod, query = final.query)
+} else {
+	GET(url = huffpo.base, path = url.premod)
+}
 
-GET(url = huffpo.base, path = url.premod, query = final.query)
 
 }
