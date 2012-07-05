0802250d7cc4c68f0b8f0b18a1d5403eec75974a
Adam Hyland <protonk@gmail.com>
2012-07-05 04:02:10 -0500

    add plyr. Comment as needed.

--- parsePoll.R
+++ parsePoll.R
@@ -1,8 +1,12 @@
+library(plyr)
+
 pollGrab <- function(page) {
   questionGrab <- function(poll) {
     responses <- poll[[1]][[5]]
     poll[[1]][[5]] <- NULL
+    # these are the poll responses. We need to preserve their structure
     resp.mat <- do.call(rbind, responses)[, 1:2]
+    # the rest do not vary across choice
     resp.list <- list(percent = resp.mat[, 2],
                       choice = resp.mat[, 1],
                       subpopulation = responses[[1]][["subpopulation"]],
@@ -10,8 +14,10 @@ pollGrab <- function(page) {
                       margin_of_error = responses[[1]][["margin_of_error"]])
     return(c(poll[[1]], resp.list))
   }
+  # select poll info for each poll
   poll.mat <- mapply(function(x) lapply(page, `[[`, x), 1:6)
   poll.list <- alply(poll.mat, .margins = 1, fun = identity)
+  # only the "questions" from each poll
   questions <- llply(page, `[[`, 7)
   questions.flat <- lapply(questions, questionGrab)
   for (i in seq_along(poll.list)) {
