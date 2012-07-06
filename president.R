2b82fd002b6552c1d749a66d4dadfe7bfe8ee644
Adam Hyland <protonk@gmail.com>
2012-07-06 18:02:24 -0500

    localize some global variables. Sum most marginal codings. Drop summed or pathologically sparse columns.

--- president.R
+++ president.R
@@ -26,12 +26,35 @@ pres.scratch <- pres.df[, reduced.names]
 ## DROP
 ## anything that drops rows goes here
 
+### Drop based by row name
+# Drop polls which are outlandishly far from 100%
+
+# Job approval
+# toss in a function to avoid global assignment
+# I'll eventually make sure each is local
+constrainApproval <- function(data) {
+  rownames(data) <- as.character(1:nrow(data))
+  approval.chart <- unique(as.character(data[grep("job-approval", data[, "chart"]), "chart"]))
+  outcome.names <- names(data)[grep("approve|mixed|undecided|neither", names(data), ignore.case = TRUE)]
+  int.sum <- rowSums(data[data[, "chart"] %in% approval.chart, outcome.names], na.rm = TRUE)
+  range.drop <- names(int.sum[int.sum > 110 | int.sum < 80])
+  # resulting rownames() have gaps
+  data[!rownames(data) %in% range.drop, ]
+}
+pres.scratch <- constrainApproval(pres.scratch)
+
 # Anything before the start of the year goes.
 pres.scratch <- pres.scratch[pres.scratch[, "avg_date"] > as.Date("2012-01-01"), ]
 # some polls don't indicate a date (this will improve as this package gets better
 # at detecting them) and aren't all that helpful
 # eventually we'll assign a guess based on id interpolation (german tank problem!)
 pres.scratch <- pres.scratch[!is.na(pres.scratch[, "avg_date"]), ]
+
+
+# Regenerate rownames after dropping
+# see http://kbroman.wordpress.com/2012/03/21/row-names-in-data-frames-beware-of-1nrow/
+
+rownames(pres.scratch) <- as.character(1:nrow(pres.scratch))
 
 ## RELEVEL
 ## Anything that changes factor levels goes here
@@ -57,4 +80,38 @@ pres.scratch[, "margin_of_error"] <- as.numeric(pres.scratch[, "margin_of_error"
 ## REIFY
 ## convert "approve, don't approve, etc" to some value for Obama and Romney
 
-
+# "obama-job-approval" is coded into "Disapprove"/"Approve"/"Undecided"
+# job approval by state is coded more expansively
+catAppVariants <- function(data) {
+  approval.chart <- unique(as.character(data[grep("job-approval", data[, "chart"]), "chart"]))
+  app.index <- data[, "chart"] %in% approval.chart
+  # group coding for approval, etc.
+  approve.names <- names(data)[grep("[^dis]approve|^approve", names(data), ignore.case = TRUE)]
+  disapprove.names <- names(data)[grep("disappp?rove", names(data), ignore.case = TRUE)]
+  neutral.names <- names(data)[grep("mixed|undecided|neither", names(data), ignore.case = TRUE)]
+  # create and fill columns
+  data[, "Sum.Job.Neutral"] <- data[, "Sum.Job.Disapprove"] <- data[, "Sum.Job.Approve"] <- NA
+  synth.cols <- c("Sum.Job.Neutral", "Sum.Job.Disapprove", "Sum.Job.Approve")
+  data[app.index, synth.cols] <- lapply(list(neutral.names,
+                                    disapprove.names,
+                                    approve.names), 
+                               function(x) rowSums(data[app.index, x], na.rm = TRUE))
+  
+  # group codes for "not voting" etc.
+  vote.names <- names(data)[grep("vot|refused", names(data), ignore.case = TRUE)]
+  data[, "Non.Response"] <- NA
+  data[, "Non.Response"] <- rowSums(data[, vote.names], na.rm = TRUE)
+  #group codes for "other"
+  data[, "Other"] <- rowSums(data[, c("Other", "Depends")], na.rm = TRUE)
+  
+  #drop columns
+  keep.cols <- names(data)[!names(data) %in% c(approve.names, disapprove.names,
+                                               neutral.names, vote.names,
+                                               "Depends", "Johnson")]
+  data[data == 0] <- NA
+  data <- data[, keep.cols]
+  return(data)
+}
+pres.out <- catAppVariants(pres.scratch)
+  
+  
