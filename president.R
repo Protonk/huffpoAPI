94ae83cc7c6221f7d47242422f2440f9fcd875d8
Adam Hyland <protonk@gmail.com>
2012-07-06 15:18:56 -0500

    break up by purpose. add releveling code.

--- president.R
+++ president.R
@@ -23,6 +23,9 @@ reduced.names <- c("id",
                    names(pres.df)[15:ncol(pres.df)])
 pres.scratch <- pres.df[, reduced.names]
 
+## DROP
+## anything that drops rows goes here
+
 # Anything before the start of the year goes.
 pres.scratch <- pres.scratch[pres.scratch[, "avg_date"] > as.Date("2012-01-01"), ]
 # some polls don't indicate a date (this will improve as this package gets better
@@ -30,15 +33,28 @@ pres.scratch <- pres.scratch[pres.scratch[, "avg_date"] > as.Date("2012-01-01"),
 # eventually we'll assign a guess based on id interpolation (german tank problem!)
 pres.scratch <- pres.scratch[!is.na(pres.scratch[, "avg_date"]), ]
 
+## RELEVEL
+## Anything that changes factor levels goes here
+
 # Easier to consider "mixed" polls as at least partially automated
 pres.scratch[pres.scratch[, "method"] == "Mixed", "method"] <- "Automated Phone"
-pres.scratch[, "method"] <- factor(as.character(pres.scratch[, "method"]))
 
+# If it changes the factor level, make sure it is in fact.cols
+fact.cols <- c("id", "pollster", "state", "method", "chart", "subpopulation")
+
+# Convert to character first to remove factors associated w/ 0 rows
+# relevel w/ lapply and assign to original columns
+pres.scratch[, fact.cols] <- as.character(unlist(pres.scratch[, fact.cols]))
+pres.scratch[, fact.cols] <- lapply(pres.scratch[, fact.cols], as.factor)
+
+## CONVERSION
+## e.g. factor to numeric, etc.
 # For state level polls we can operate on this as a factor but w/ >400 levels
 # it doesn't make sense
 pres.scratch[, "observations"] <- as.numeric(pres.scratch[, "observations"])
 pres.scratch[, "margin_of_error"] <- as.numeric(pres.scratch[, "margin_of_error"])
 
-# relevel "chart" and drop some levels
-pres.scratch[, "chart"] <- factor(as.character(pres.scratch[, "chart"]))
+## REIFY
+## convert "approve, don't approve, etc" to some value for Obama and Romney
+
 
