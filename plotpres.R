01fd9a91222fa64e8424575c522d38e7192056dd
Adam Hyland <protonk@gmail.com>
2012-07-07 13:23:55 -0500

    national presidential poll plotting

---
+++ plotpres.R
@@ -0,0 +1,16 @@
+library(ggplot2)
+pres.out <- pres.scratch
+by.state.df <- data.frame(state = tolower(state.name),
+                          Obama = NA)
+state.polls <- ddply(pres.out, "state", summarise, 
+                     Obama = mean(Obama, na.rm = TRUE))
+state.polls <- state.polls[state.polls[, "state"] != "US", ]
+state.polls[, "state"] <- tolower(state.name[state.abb %in% state.polls[, "state"]])
+
+by.state.df[by.state.df[, "state"] %in% state.polls[, "state"], ] <- state.polls
+by.state.df[, "state"] <- factor(by.state.df[, "state"])
+
+states_map <- map_data("state")
+
+ggplot(by.state.df, aes(map_id = state)) + geom_map(aes(fill = Obama), map = states_map) + expand_limits(x = states_map$long, y = states_map$lat)
+
