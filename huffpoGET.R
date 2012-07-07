2b044e0afc14c09e38da76f14a2449a49f120008
Adam Hyland <protonk@gmail.com>
2012-07-07 00:11:04 -0500

    use fromJSON's null handler like a normal person. Integrate the call to RJSONIO into the url generator (use the friendly gen if you want to get at the api directly)

--- huffpoGET.R
+++ huffpoGET.R
@@ -1,3 +1,4 @@
+library(RJSONIO)
 ## function to build GET requests for charts and polls
 # builds the path (minus queries) so we don't have to worry about it later
 huffurl <- function(path, format) {
@@ -7,15 +8,14 @@ huffurl <- function(path, format) {
   return(paste(huffpo, path, sep = "/"))
 }
 
-# Shorter function to generate queries
-# flexible query construction, which allows the user to pass
-# inalid queries. But it isn't in the way
+# Generate R objects from JSON, given path names
 
-huffpoSlim <- function(path, format = "json", ...) {
+huffpoJSON <- function(path, format = "json", ...) {
   # Filter() is awesome. Drop null optional arguments and
   # preserve ordering (not important here, but still)
   arglist <- Filter(length, list(...))
   # Names on the left, values on the right
   query <- paste(names(arglist), sapply(arglist, `[`), sep = "=", collapse = "&")
-  return(paste(huffurl(path, format), query, sep = "?"))
+  full.url <- paste(huffurl(path, format), query, sep = "?")
+  fromJSON(full.url, nullValue = NA)
 }
